b6b22e1792bc259f7c1c3b13bf0d02161d69abc8
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-24 21:19:21 +0900

    Feat : Add user post method router
    유저를 등록하는 post 메서드 추가

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,5 @@
-const { readUserData } = require('../db/instructions/user');
+const { readUserData, createUser, isDupleUser } = require("../db/instructions/user"); 
+const encrpyPassword = require('../utils/encrypt');
 
 const controller = {
   // 유저 가져오기
@@ -6,11 +7,29 @@ const controller = {
     try {
       const { id } = req.params;
       const user = await readUserData(id);
-      return res.json({user});
+      return res.json({ user });
     } catch (error) {
       next(error);
     }
   },
+  // 유저 생성하기
+  createUser: async (req, res, next) => {
+    try {
+      let { name, email, password } = req.body;
+      
+      // 유저 중복 체크
+      if (await isDupleUser(email)) {
+        return res.status(400).json({ msg : 'User already exist' });
+      }
+
+      password = await encrpyPassword(password);
+      const user = await createUser({ name, email, password });
+
+      return res.json({ user });
+    } catch (error) {
+      next(error);
+    }
+  }
 };
 
 module.exports = controller;
