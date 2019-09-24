765f7055f75b65f41066115998f35da45ce86005
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-24 21:18:06 +0900

    Feat : Add createUser, isDupleUser method
    중복된 유저가 있는 지 확인하는 isDupleUser,
    유저를 db에 insert하는 createUser method 추가

--- server/db/instructions/user.js
+++ server/db/instructions/user.js
@@ -1,9 +1,23 @@
 const { User } = require("../models");
 
 const instructions = {
-  readUserData : async id => {
+  readUserData: async id => {
     const user = await User.findOne({ where: { id } });
     return user;
+  },
+
+  createUser: async ({ name, email, password }) => {
+    const user = await User.create({ name, email, password });
+    return user;
+  },
+
+  isDupleUser: async email => {
+    let flag = false;
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+      flag = true;
+    }
+    return flag;
   }
 };
 
