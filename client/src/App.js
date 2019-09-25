dc76fdc02c9c7c6240a08eb9efbec6e69ae46a83
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-25 18:59:33 +0900

    Init : semantic-ui component, css 세팅 및 초기화

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 import "./App.css";
+import 'semantic-ui-css/semantic.min.css'
 
 const App = () => {
-  return <div className="App">My App</div>;
+  return (
+    <div className="App">
+      <Button primary>this is primary</Button>
+    </div>
+  );
 };
 
 export default App;
