f7fffe6bd923595f9c4557cbc674f81874e2a929
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-25 17:13:23 +0900

    Feat : authRouter 추가

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,9 @@
 const express    = require('express'),
       router     = express.Router(),
-      userRouter = require('./api/user');
+      userRouter = require('./api/user'),
+      authRouter = require('./api/auth');
 
 router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 module.exports = router;
