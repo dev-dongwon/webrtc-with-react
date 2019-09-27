dd433e5bf9712f949e7755eeacbbdb74c1ef2790
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-27 15:02:55 +0900

    Modified : 컴포넌트 변경 및 수정 내용 반영

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import SideMenu from "./components/layout/menu/SideMenu";
 import { Grid, CssBaseline } from "@material-ui/core";
 import ContentArea from "./components/layout/content/ContentArea";
+import AuthState from "./context/auth/AuthState";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Grid container component="main">
-        <CssBaseline />
-        <SideMenu />
-        <ContentArea />
-      </Grid>
-    </Router>
+    <AuthState>
+      <Router>
+        <Grid container>
+          <CssBaseline />
+          <SideMenu />
+          <ContentArea />
+        </Grid>
+      </Router>
+    </AuthState>
   );
 };
 
