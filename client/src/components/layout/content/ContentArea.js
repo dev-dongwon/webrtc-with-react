79490c28bc3f473f137757e3170324a89c0cc469
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-27 14:54:21 +0900

    Feat : Switch router 추가
    로그인, 회원가입 라우터 추가

--- client/src/components/layout/content/ContentArea.js
+++ client/src/components/layout/content/ContentArea.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Grid, Paper } from "@material-ui/core";
-import Signup from "../../pages/Signup";
-import Login from "../../pages/Login";
+import Signup from "../../auth/Signup";
+import Login from "../../auth/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const ContentArea = () => {
   return (
-    <Grid xs={8} sm={8} md={10} className="content">
-      <Switch>
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Grid>
+    <Fragment>
+      <Grid item xs={2} sm={2} md={2}></Grid>
+      <Grid item xs={8} sm={8} md={10} className="content">
+        <Switch>
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Grid>
+    </Fragment>
   );
 };
 
