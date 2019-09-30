e088c5773e1a624fd67aa4e08f5596dd2f2477bc
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 02:55:19 +0900

    Feat : production 환경을 위해 build된 파일 업로드

---
+++ client/build/precache-manifest.baaa29c0bf4a89e545f6f5d6b4cf5733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c667038a886bc69e72b0581c51bcd11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13cae28a5932f8a814cd",
+    "url": "/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "c0f079751c5a150887e6",
+    "url": "/static/css/main.398166ea.chunk.css"
+  },
+  {
+    "revision": "13cae28a5932f8a814cd",
+    "url": "/static/js/2.2b32b15e.chunk.js"
+  },
+  {
+    "revision": "c0f079751c5a150887e6",
+    "url": "/static/js/main.c5a0e7c3.chunk.js"
+  },
+  {
+    "revision": "09e27f03fdb94530568a",
+    "url": "/static/js/runtime-main.9d4f7790.js"
+  }
+]);
