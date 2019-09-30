65926ecfff4606bdd0b5ce86a7d8f74797a6f331
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-30 19:01:51 +0900

    Feat : 채팅 입력창, 출력창 컴포너트 디자인

--- client/src/components/room/Room.js
+++ client/src/components/room/Room.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Player, ControlBar } from "video-react";
+import { Player } from "video-react";
 import "../../../node_modules/video-react/dist/video-react.css";
 import {
-  Button,
   Divider,
-  Modal,
-  TextField,
-  MenuItem,
-  Paper
+  Paper,
+  Chip,
+  Typography,
+  Button,
+  TextField
 } from "@material-ui/core";
 import uuid from "uuid";
 
@@ -17,50 +17,98 @@ const useStyles = makeStyles(theme => ({
   root: {
     margin: "1rem",
     padding: theme.spacing(3, 2),
-    height: "auto"
+    height: "96vh"
   },
   flex: {
     display: "flex"
   },
   topicsWindow: {
     width: "80%",
-    height: "auto",
+    height: "auto"
   },
   chatWindow: {
-    width: "20%",
-    height: "auto"
+    width: "350px",
+    height: "95vh"
   },
   chatBox: {
-    width: "85%"
+    height: "85%",
+    backgroundColor: "white"
+  },
+  messageBox: {
+    height: "10%",
+    backgroundColor: "white"
   },
   button: {
-    width: "15%"
+    marginTop: "10px",
+    textAlign: "right"
   },
-  video: {
-    width: "50%",
-    height: "auto"
+  chip: {
+    marginLeft: "1rem",
+    marginRight: "1rem"
+  },
+  message: {
+    alignItems: "center"
+  },
+  messageWrapper: {
+    padding: "5px"
+  },
+  textBox: {
+    width: "330px"
   }
 }));
 
 const Room = ({ match }) => {
   const classes = useStyles();
+  const [textValue, changeTextValue] = useState("");
 
   return (
     <div>
       <Paper className={classes.root}>
-        <div>{match.params.roomId}</div>
         <div className={classes.flex}>
           <div className={classes.topicsWindow}>
             <Player
               autoPlay={false}
-              className={classes.video}
               src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
             />
+            <div className={classes.flex}>
+              <div>{match.params.roomId}</div>
+            </div>
           </div>
-          <div className={classes.chatWindow}></div>
-        </div>
-        <div className={classes.flex}>
-          <div className={classes.chatBox}></div>
+          <div className={classes.chatWindow}>
+            <div className={classes.chatBox}>
+              {[
+                { from: "user", msg: "hello" },
+                { from: "user", msg: "hello" }
+              ].map((chat, i) => {
+                return (
+                  <div
+                    className={classes.flex}
+                    key={i}
+                    className={classes.messageWrapper}
+                  >
+                    <Chip label={chat.from} className={classes.chip} />
+                    {chat.msg}
+                  </div>
+                );
+              })}
+            </div>
+            <div className={classes.messageBox} className={classes.button}>
+              <div>
+                <TextField
+                  id="standard-name"
+                  className={classes.textBox}
+                  placeholder="Send a chat"
+                  value={textValue}
+                  onChange={e => changeTextValue(e.target.value)}
+                />
+              </div>
+              <div className={classes.button}>
+                <Button variant="contained" color="primary">
+                  send
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </Paper>
     </div>
