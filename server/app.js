31886f16dba75e0e630edb32e87592089e839cde
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 17:45:31 +0900

    Fix : build 파일 path 수정

--- server/app.js
+++ server/app.js
@@ -14,10 +14,10 @@ const express = require("express"),
 const app = express();
 
 // middlewares
+app.use(express.static(path.join(__dirname, "./../client", "build")));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 // routes
 const apiRouter = require("./routes/api");
