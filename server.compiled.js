a94b2eebb832dede572f6230707582efba1fa014
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 18:09:36 +0900

    Fix : script 수정 및 server rebuild

--- server.compiled.js
+++ server.compiled.js
@@ -4,11 +4,6 @@ var http = require('http');
 
 var app = require('./server/app');
 
-var express = require('express');
-
-var path = require('path');
-
-app.use(express["static"](path.join(__dirname, 'client', 'build')));
 var PORT = process.env.PORT || 5000;
 var server = http.createServer(app);
 server.listen(PORT);
