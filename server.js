528b7d8dbaaeefe9e9a08d80387b16e9c359743f
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 17:41:59 +0900

    Fix : 정적 파일 읽는 메서드 app.js로 이동

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const http = require('http');
 const app = require('./server/app');
-const express = require('express');
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 const PORT = process.env.PORT || 5000;
 
--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 
 // sequelize
-const sequelize = require('./db/models').sequelize;
+const sequelize = require("./db/models").sequelize;
 sequelize.sync();
 
 // import modules
 const express = require("express"),
       morgan  = require("morgan"),
-      helmet  = require("helmet");
+      helmet  = require("helmet"),
+      path    = require("path");
 
 // server application
 const app = express();
@@ -16,9 +17,10 @@ const app = express();
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // routes
-const apiRouter = require('./routes/api');
+const apiRouter = require("./routes/api");
 
 // routes
 app.use("/api", apiRouter);
