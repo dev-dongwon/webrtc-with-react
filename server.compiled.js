3b2e37a2ecdefc39f8660afd008d3bcc0d6bc92e
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 21:50:40 +0900

    Modified : dev 환경으로 백업

--- server.compiled.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var http = require('http');
-
-var app = require('./server/app');
-
-var PORT = process.env.PORT || 5000;
-var server = http.createServer(app);
-server.listen(PORT);
