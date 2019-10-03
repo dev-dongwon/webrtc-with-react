98a07c77b764003acdb72edf9972def9726f7a69
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-03 14:06:31 +0900

    Feat : 클라이언트와 socket 통신 확인
    클라이언트의 socket 데이터를 받기 위한 코드 추가

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const http = require('http');
-const app = require('./server/app');
+const http = require("http");
+const app = require("./server/app");
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+const io = require("socket.io")(server, { origins: "*:*" });
+
+io.on("connection", socket => {
+  console.log("user connect");
+  socket.on("chat", msgObj => {
+    console.log(msgObj);
+  });
+});
+
 server.listen(PORT);
