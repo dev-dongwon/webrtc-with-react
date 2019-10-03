25573feafd01e4824c4543606bf3c79640664585
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-03 14:13:08 +0900

    Feat : 메시지를 받기 위한 receive message type 추가
    reducer에서 처리할 메시지 처리 타입 추가

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -11,3 +11,6 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+/* chat type */
+export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
