1ddab903d14abd68eef1dbc283ac1d0b7558c1a3
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-06 16:05:26 +0900

    Feat : namepsace instance 생성 유틸 추가

---
+++ client/src/utils/namespaces.js
@@ -0,0 +1,8 @@
+import Namespace from "../chatclasses/Namespace";
+
+const namespaceInatances = {
+  0: new Namespace(0, "talk", "/talk"),
+  1: new Namespace(1, "jam", "/jam")
+};
+
+export default namespaceInatances;
