151fa30900be4a489ab0967542b4c748ed8b9970
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-07 20:10:58 +0900

    Fix : loadUser를 userEffect에 추가

--- client/src/components/layout/menu/SideMenu.js
+++ client/src/components/layout/menu/SideMenu.js
@@ -24,7 +24,7 @@ const Nav = () => {
     if (token && !user) {
       loadUser();
     }
-  }, [token, user]);
+  }, [token, user, loadUser]);
 
   return (
     <Grid item xs={false} sm={4} md={2} className={classes.nav}>
