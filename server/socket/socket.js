3f62c5a9d2fd88b62fe82fd99d4c0a69e43536fb
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-12 12:07:28 +0900

    Feat : socket에 chat Event, webRTC sdp 교환 Event 추가
    socket connection 후 chat, stream 획득을 위한 sdp 교환 이벤트 추가

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -6,21 +6,76 @@ const socketConnect = io => {
 
     nspSocket.on("connection", socket => {
       const username = socket.handshake.query.username;
+      const roomId = socket.handshake.query.roomId;
 
-      socket.on("join", (roomId, callback) => {
-        socket.join(roomId);
+      joinEvent(socket, nspSocket, username);
+      enterEvent(socket, nspSocket, roomId, username);
+      chatEvent(socket, nspSocket);
+      mySocketEvent(socket);
+      webRtcOfferEvent(socket, nspSocket);
+      webRtcAnswerEvent(socket, nspSocket);
 
-        nspSocket.in(roomId).clients((err, clients) => {
-          console.log(clients.length);
-          callback({ clientLength: clients.length, username });
-        });
-      });
-
-      socket.on("chat", data => {
-        const { roomId, from, msg } = data;
-        nspSocket.in(roomId).emit("chat", { from, msg });
+      socket.on("close", () => {
+        socket.leave(socket.handshake.query.roomId);
       });
     });
+  });
+};
+
+const webRtcAnswerEvent = (socket, nspSocket) => {
+  socket.on("answer", ({ data, mySocketId, targetSocketId }) => {
+    nspSocket.to(targetSocketId).emit("backAnswer", {
+      answer: data,
+      targetSocketId: mySocketId,
+      mySocketId: targetSocketId
+    });
+  });
+};
+
+const webRtcOfferEvent = (socket, nspSocket) => {
+  socket.on("offer", ({ data, mySocketId, targetSocketId }) => {
+    nspSocket.to(targetSocketId).emit("backOffer", {
+      offer: data,
+      targetSocketId: mySocketId,
+      mySocketId: targetSocketId
+    });
+  });
+};
+
+const mySocketEvent = socket => {
+  socket.on("mySocket", () => {
+    socket.emit("socketAnswer", socket.id);
+  });
+};
+
+const joinEvent = (socket, nspSocket, username) => {
+  socket.on("join", roomId => {
+    socket.join(roomId);
+    nspSocket.in(roomId).emit("join", {
+      from: "server:entrance",
+      msg: username
+    });
+    nspSocket.in(roomId).emit("enter");
+  });
+};
+
+const enterEvent = (socket, nspSocket, roomId, username) => {
+  socket.on("enter", () => {
+    const user = { username, socketId: socket.id };
+
+    nspSocket.in(roomId).clients((err, clients) => {
+      nspSocket.in(roomId).emit("sendUserInfo", {
+        user: user,
+        userNumbers: clients.length
+      });
+    });
+  });
+};
+
+const chatEvent = (socket, nspSocket) => {
+  socket.on("chat", data => {
+    const { roomId, from, msg } = data;
+    nspSocket.in(roomId).emit("chat", { from, msg });
   });
 };
 
