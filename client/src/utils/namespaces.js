a055bcd92584adb3649cc7d5c098c72865a68181
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-12 12:03:22 +0900

    Fix : webRTC stream 유실문제로 room component 통합
    video component로 나눈 것을 room component로 통합
    webRTC stream 유실 문제 해결

--- client/src/utils/namespaces.js
+++
@@ -1,8 +0,0 @@
-import Namespace from "../chatclasses/Namespace";
-
-const namespaceInatances = {
-  0: new Namespace(0, "talk", "/talk"),
-  1: new Namespace(1, "jam", "/jam")
-};
-
-export default namespaceInatances;
