15c7c0789695da4238bb55b93c24e4f2a949f53e
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-12 12:18:16 +0900

    Feat : Room State 추가

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 import LobbyState from "./context/lobby/LobbyState";
 import RoomState from "./context/room/RoomState";
-import VideoState from "./context/video/VideoState";
 import "./App.css";
 
 const App = () => {
@@ -16,15 +15,13 @@ const App = () => {
       <AuthState>
         <LobbyState>
           <RoomState>
-            <VideoState>
-              <Router>
-                <Grid container>
-                  <CssBaseline />
-                  <SideMenu />
-                  <ContentArea />
-                </Grid>
-              </Router>
-            </VideoState>
+            <Router>
+              <Grid container>
+                <CssBaseline />
+                <SideMenu />
+                <ContentArea />
+              </Grid>
+            </Router>
           </RoomState>
         </LobbyState>
       </AuthState>
