98edd34d389bdfa8650dc9e81940062d12a90cb9
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-14 19:11:55 +0900

    Feat : beanstalk 배포 환경에 맞춰 환경변수, 포트 변경

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./server/app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8081;
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, { origins: "*:*" });
--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,28 +1,28 @@
 const development = {
-  username: process.env.MYSQL_DEV_USERNAME,
-  password: process.env.MYSQL_DEV_PASSWORD,
-  database: process.env.MYSQL_DEV_DATABASE,
-  host: process.env.MYSQL_DEV_HOST,
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: "ebdb",
+  host: process.env.RDS_HOSTNAME,
   dialect: "mysql",
-  port: process.env.MYSQL_DEV_PORT
+  port: process.env.RDS_PORT
 };
 
 const production = {
-  username: process.env.MYSQL_PRO_USERNAME,
-  password: process.env.MYSQL_PRO_PASSWORD,
-  database: process.env.MYSQL_PRO_DATABASE,
-  host: process.env.MYSQL_PRO_HOST,
-  dialect: process.env.MYSQL_PRO_DIALECT,
-  port: process.env.MYSQL_PRO_PORT
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: "ebdb",
+  host: process.env.RDS_HOSTNAME,
+  dialect: "mysql",
+  port: process.env.RDS_PORT
 };
 
 const test = {
-  username: process.env.MYSQL_TEST_USERNAME,
-  password: process.env.MYSQL_TEST_PASSWORD,
-  database: process.env.MYSQL_TEST_DATABASE,
-  host: process.env.MYSQL_TEST_HOST,
-  dialect: process.env.MYSQL_TEST_DIALECT,
-  port: process.env.MYSQL_TEST_PORT
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: "ebdb",
+  host: process.env.RDS_HOSTNAME,
+  dialect: "mysql",
+  port: process.env.RDS_PORT
 };
 
 module.exports = { development, production, test };
