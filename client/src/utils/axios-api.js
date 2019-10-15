af54b42a48f3e5b8fd0cd95d233cf7196b88dc44
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-15 13:09:41 +0900

    Fix : cors error 문제 해결을 위해 base url을 https로 수정

--- client/src/utils/axios-api.js
+++ client/src/utils/axios-api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://server-dev.ap-northeast-2.elasticbeanstalk.com/"
+  baseURL: "https://server-dev.ap-northeast-2.elasticbeanstalk.com/"
 });
 
 export default axios;
